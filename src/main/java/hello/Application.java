0fc21928145c8b2b81ea2b77b1de01b73696c70c
Minh <minhlevan104@gmail.com>
2018-09-05 17:06:34 +0700

    upload file

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,14 +1,32 @@
 package hello;
 
+import hello.storage.StorageProperties;
+import hello.storage.StorageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
-@EnableScheduling
+@EnableConfigurationProperties(StorageProperties.class)
 public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
     }
 }

9731fac26d49229f36a060f9cb98f21703fb2632
Minh <minhlevan104@gmail.com>
2018-09-06 17:08:53 +0700

    actuator-service

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,32 +1,19 @@
-package hello;
-
-import hello.storage.StorageProperties;
-import hello.storage.StorageService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
-
-@SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
-public class Application {
-
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    CommandLineRunner init(StorageService storageService) {
-        return (args) -> {
-            storageService.deleteAll();
-            storageService.init();
-        };
-    }
-}
+//package hello;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class Application {
+//
+//    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+//
+//
+//    public static void main(String[] args) throws Exception {
+//         SpringApplication.run(Application.class, args);
+//
+//    }
+//}
